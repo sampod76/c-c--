f15aaba78e8deeecde55effadb90087f07a4e33a
sampod76 <sampodnath76@gmail.com>
2024-10-15 01:18:02 +0600

    add c

---
+++ c/forstCode.c
@@ -0,0 +1,6 @@
+#include <stdio.h> // standerd intput output
+int main()
+{
+    printf("Hollo world");
+    return 0;
+}
---
+++ text.c
@@ -0,0 +1,30 @@
+// https://phitron.io/ph026/video/ph026-1-3-basic-structure-of-a-c-program7
+#include <stdio.h>
+int main()
+{
+    printf("Welcome");
+    return 0; // main function returns 0 to indicate successful execution
+}
+
+//-------- linking section / preprocessor section
+#include <stdio.h>
+#include <math.h>
+
+//-------definition section-------
+#define PI 3.1416
+#define ll long long
+
+// global declaration section-------
+int N = 100;
+int sum(int x, int y)
+
+    //---------main function section-------
+    int main()
+{
+    return 0;
+}
+//---sub program section-------
+int sum(int x, int y)
+{
+    return x + y;
+}
