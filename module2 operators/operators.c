b6a202d954526cbd2ce5d4e9c30217fc333a6bd3
sampod76 <sampodnath76@gmail.com>
2024-10-15 01:47:41 +0600

    operator

--- module2 operators/operators.c
+++ module2 operators/operators.c
@@ -3,6 +3,25 @@
 
 int main()
 {
-    printf("%d", 3 + 2);
+    printf("%d\n", 3 + 2); // 5
+    // printf("%f", 3 + 2);   // 5
+
+    long long a = 10000, b = 100000000000000;
+    long long result = a * b;
+    printf("%lld\n", result); // 1000000000000000000
+
+    // alternative
+    int c = 10000, d = 100000000000000;
+    int result2 = 1LL * c * d;
+    printf("%lld\n", result2); // 1000000000000000000
+
+    //
+    double e = 3, f = 2.12;
+    double result3 = e * f;
+    printf("%.2f\n", result3); // 8.77
+
+    //
+    int i = 10, j = 2.5;
+    printf("%.10f\n", i / j); //
     return 0;
 }
